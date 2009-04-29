9638156537306c8d30f3a0a05d64c2cc4ca0d465
pwoods <pwoods@fool.com>
2009-04-29 17:32:10 -0400

    custom.el changes

--- custom.el
+++ custom.el
@@ -4,6 +4,7 @@
   ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
+ '(ac-sources (quote (ac-source-yasnippet ac-source-abbrev ac-source-words-in-buffer)) t)
  '(blink-cursor-mode nil)
  '(company-idle-delay 0.5)
  '(company-tooltip-delay 0.5)
@@ -37,4 +38,5 @@
   ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
+ '(mumamo-background-chunk-major ((((class color) (min-colors 88) (background dark)) nil)))
  '(mumamo-background-chunk-submode ((((class color) (min-colors 88) (background dark)) (:background "black1")))))
