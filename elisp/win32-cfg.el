e050fc814190b1781e98d47f17455fe842fc1445
pwoods <pwoods@fool.com>
2009-06-08 13:56:33 -0400

    Defautl frame sizes for work box

--- elisp/win32-cfg.el
+++ elisp/win32-cfg.el
@@ -12,6 +12,11 @@
 (if (at-work-p)
     (progn (set-default-font
 	    "-outline-Bitstream Vera Sans Mono-normal-r-normal-normal-13-97-96-96-c-*-iso8859-1")
+	   ;;=Initial Frame Sizes
+	   (setq initial-frame-alist '((top . 10 ) (left . 20) 
+				       (width . 150) (height . 60)))
+	   (setq default-frame-alist '((top .10) (left . 20) 
+				       (width . 150) (height . 60)))
 	   (require 'compress)))
 
 (provide 'win32-cfg)
