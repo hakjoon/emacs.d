555d92f9e0054b45df96741367fb35ddf29f7df6
pwoods <pwoods@fool.com>
2009-12-03 11:23:24 -0500

    Updated yasnippet config

--- config/yasnippet-cfg.el
+++ config/yasnippet-cfg.el
@@ -2,7 +2,7 @@
 
 (require 'yasnippet)
 (yas/initialize)
-(yas/load-directory "~/.emacs.d/vendor/yasnippet-0.6.1c/snippets/")
-(yas/load-directory "~/.emacs.d/my/snippets/")
+(yas/load-directory "~/.emacs.d/vendor/yasnippet-0.6.1c/snippets")
+(setq yas/root-directory "~/.emacs.d/my/snippets")
 
 (provide 'yasnippet-cfg)
