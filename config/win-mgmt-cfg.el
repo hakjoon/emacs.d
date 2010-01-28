891545cfda466e102af7263f0ddaf616c31b96b8
hakjoon <public@hakjoon.com>
2010-01-27 23:00:34 -0500

    actually adding the files

---
+++ config/win-mgmt-cfg.el
@@ -0,0 +1,13 @@
+;; Setting related to window management
+
+;; winner-mode
+(winner-mode t)
+
+;; windmove bindings on
+(windmove-default-keybindings)
+
+;; winring
+(require 'winring)
+(winring-initialize)
+
+(provide 'win-mgmt-cfg.el)
