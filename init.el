08a315eeec1db525d57635e9b61a19ae83e80ce1
hakjoon <public@hakjoon.com>
2010-06-29 00:06:01 -0400

    moving to new pacakge.el and breaking elpa out ala starter kit

--- init.el
+++ init.el
@@ -1,24 +1,20 @@
 ;; server
 (server-start)
 
-;;; This was installed by package-install.el.
-;;; This provides support for the package system and
-;;; interfacing with ELPA, the package archive.
-;;; Move this code earlier if you want to reference
-;;; packages in your .emacs.
-(when
-    (load
-     (expand-file-name "~/.emacs.d/elpa/package.el"))
-  (package-initialize))
+(setq dotfiles-dir (file-name-directory
+                    (or (buffer-file-name) load-file-name)))
 
 ;; Search subdirectories of ~/.emacs.d/elisp for .el files
 (if (fboundp 'normal-top-level-add-subdirs-to-load-path)
-    (let* ((my-lisp-dir "~/.emacs.d")
+    (let* ((my-lisp-dir dotfiles-dir)
 	   (default-directory my-lisp-dir))
       (setq load-path (cons my-lisp-dir load-path))
       (normal-top-level-add-subdirs-to-load-path)))
 
-
+;; ELPA
+(require 'package)
+(package-initialize)
+(setq package-user-dir (concat dotfiles-dir "elpa"))
 
 ;; configuration stuff
 (require 'master-cfg)
