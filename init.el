3fdbc7a3cce29afce9ff302dda35ad92f56ee0a3
hakjoon <public@hakjoon.com>
2010-06-29 00:15:40 -0400

    add elpa to hgignore, clean up load path loader

--- init.el
+++ init.el
@@ -1,14 +1,14 @@
 ;; server
 (server-start)
 
+;; set .emacs.d folder path
 (setq dotfiles-dir (file-name-directory
                     (or (buffer-file-name) load-file-name)))
 
-;; Search subdirectories of ~/.emacs.d/elisp for .el files
+;; Search subdirectories of dotfiles-dir for .el files
 (if (fboundp 'normal-top-level-add-subdirs-to-load-path)
-    (let* ((my-lisp-dir dotfiles-dir)
-	   (default-directory my-lisp-dir))
-      (setq load-path (cons my-lisp-dir load-path))
+    (let* ((default-directory dotfiles-dir))
+      (setq load-path (cons dotfiles-dir load-path))
       (normal-top-level-add-subdirs-to-load-path)))
 
 ;; ELPA
