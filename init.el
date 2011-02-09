926ed3076d9739c4969fd82a83efef8d67b85667
Patrick Woods <pwoods@fool.com>
2011-02-09 15:12:14 -0500

    Fix server security errors and set new projects path

--- init.el
+++ init.el
@@ -1,6 +1,3 @@
-;; server
-(server-start)
-
 ;; set .emacs.d folder path
 (setq dotfiles-dir (file-name-directory
                     (or (buffer-file-name) load-file-name)))
@@ -23,3 +20,14 @@
 (put 'narrow-to-region 'disabled nil)
 
 (put 'upcase-region 'disabled nil)
+
+;;; EmacsClient
+
+(require 'server)
+(when (and (= emacs-major-version 23)
+           (= emacs-minor-version 1)
+           (equal window-system 'w32))
+  (defun server-ensure-safe-dir (dir) "Noop" t)) ; Suppress error "directory
+                                                 ; ~/.emacs.d/server is unsafe"
+                                                 ; on windows.
+(server-start 1)
