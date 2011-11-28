f2dc2199404b88b5a45d29cc6af8aeb082b7b815
hakjoon <public@hakjoon.com>
2011-11-27 22:53:03 -0500

    fixed python-flymake

--- config/python-cfg.el
+++ config/python-cfg.el
@@ -18,11 +18,8 @@
 		     'flymake-create-temp-inplace))
 	 (local-file (file-relative-name
 		      temp-file
-		      (file-name-directory buffer-file-name)))
-	 (py-flymake-dir (expand-file-name (concat dotfiles-dir "support-apps/py-flymake/")))
-	 (py-flymake-command (concat py-flymake-dir "bin/python"))
-	 (py-flymake-script (concat py-flymake-dir "pyflymake.py")))
-    (list py-flymake-command (list py-flymake-script local-file))))
+		      (file-name-directory buffer-file-name))))
+    (list "python" (list (expand-file-name (concat dotfiles-dir "support-apps/py-flymake/pyflymake.py")) local-file))))
 
 (defun flymake-python-load ()
   (interactive)
@@ -38,7 +35,8 @@
 	    (imenu-add-to-menubar "Browser")
 	    (setq indent-tabs-mode nil)
 	    (setq python-indent-offset 4)
-	    (flymake-python-load)))
+	    (flymake-python-load)
+))
 
 (message "locading python-cfg")
 (provide 'python-cfg)
