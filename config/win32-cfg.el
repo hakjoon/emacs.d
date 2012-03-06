a17764335e9dbf95e1e1de1f29e4ff5602537907
Patrick Woods <pwoods@fool.com>
2012-03-06 10:19:32 -0500

    Changing root project location for windows

--- config/win32-cfg.el
+++ config/win32-cfg.el
@@ -20,7 +20,7 @@
 	   ;;=Initial Frame Sizes
 	   (setq initial-frame-alist '((top . 10 ) (left . 20) 
 				       (width . 150) (height . 60)))
-	   (cd "c:/Projects/Fool/")
+	   (cd "c:/Projects")
 	   ;;(setq default-frame-alist initial-frame-alist)
 	   (require 'compress)))
 
