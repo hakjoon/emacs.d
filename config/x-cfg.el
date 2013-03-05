910019bd0eae77bd1ba0bb016e6158a0a965b4ef
Patrick Woods <pwoods@fool.com>
2013-03-05 13:52:53 -0500

    fix size and rec

--- config/x-cfg.el
+++ config/x-cfg.el
@@ -1,8 +1,8 @@
 (set-default-font "-unknown-DejaVu Sans Mono-normal-normal-normal-*-13-*-*-*-m-0-iso10646-1")
 
 (setq initial-frame-alist '((top . 10 ) (left . 20) 
-				       (width . 150) (height . 60)))
+				       (width . 150) (height . 50)))
 
-(cd "~/Projects")
+(cd "~/projects")
 
 (provide 'x-cfg)
