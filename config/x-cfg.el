b921c1d2e8134699a3bf17a1f7651e32419471b8
Patrick Woods <pwoods@fool.com>
2013-06-04 22:19:58 -0400

    tweak x-cfg height

--- config/x-cfg.el
+++ config/x-cfg.el
@@ -1,7 +1,7 @@
 (set-default-font "-unknown-DejaVu Sans Mono-normal-normal-normal-*-13-*-*-*-m-0-iso10646-1")
 
 (setq initial-frame-alist '((top . 10 ) (left . 20) 
-				       (width . 150) (height . 50)))
+				       (width . 150) (height . 48)))
 
 (cd "~/Projects")
 
